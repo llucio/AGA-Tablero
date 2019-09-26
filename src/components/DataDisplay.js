54410e93aa09fcc6ee2e7b75d75754a2e3588c2b
David Regla <dreglad@gmail.com>
2019-09-26 09:59:13 -0500

    :bookmark: v0.1.2

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,21 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { get, keys, has, isObject, isArray, isBoolean, isString } from 'lodash';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 const DataDisplay = ({ data, ...props }) => {
   const { keyLabels, labelComponent = 'h3', valueComponent = Col } = props;
-  if (isArray(data)) {
-    return (
-      <Row>
-        {data.map(value => (
-          <valueComponent key={value}>
-            <DataDisplay key={value} data={value} {...props} />
-          </valueComponent>
-        ))}
-      </Row>
-    );
-  } else if (isObject(data)) {
+
+  // Un objeto key/value renderea llave y un DataDisplay recursivo con el valor
+  if (isObject(data)) {
     const dataKeys = keys(keyLabels || data).filter(key => has(data, key));
     return dataKeys.map(key => (
       <Row key={key}>
@@ -27,14 +20,43 @@ const DataDisplay = ({ data, ...props }) => {
         </Col>
       </Row>
     ));
-  } else if (isBoolean(data)) {
+  }
+
+  // Una lista de valores renderea DataDisplay recursivamente
+  else if (isArray(data)) {
+    return (
+      <Row>
+        {data.map(value => (
+          <valueComponent key={value}>
+            <DataDisplay key={value} data={value} {...props} />
+          </valueComponent>
+        ))}
+      </Row>
+    )
+  }
+
+  // Un Boolean renderea el texto "Sí" o "No"
+  else if (isBoolean(data)) {
     return <DataDisplay data={data ? 'Sí' : 'No'} {...props} />;
-  } else if (isString(data)) {
+  }
+
+  // Un String renderea un párrafo
+  else if (isString(data)) {
     return <p>{data}</p>;
-  } else {
+  }
+
+  // Valor de tipo desconocido
+  else {
     console.error('Unexpected data', data);
     return null;
   }
+};
+
+DataDisplay.propTypes = {
+  data: PropTypes.any.isRequired,
+  keyLabels: PropTypes.object,
+  labelComponent: PropTypes.elementType,
+  valueComponent: PropTypes.elementType
 };
 
 export default DataDisplay;
