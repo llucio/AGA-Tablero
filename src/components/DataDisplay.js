36127b7e1b171d2ab8d01c6e5c1b60a0feb12415
David Regla <dreglad@gmail.com>
2019-10-13 23:27:37 -0500

    :art: Componente DataDisplay más legible

--- src/components/DataDisplay.js
+++ src/components/DataDisplay.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  get,
-  keys,
-  has,
-  isPlainObject,
-  isArray,
-  isBoolean,
-  isString
-} from 'lodash';
+import _ from 'lodash';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import RawHtml from './RawHtml';
 
+/**
+ * Componente para despliegue genérico recursivo de datos
+ */
 const DataDisplay = ({ data, ...rest }) => {
   const {
     keyLabels,
@@ -21,12 +16,12 @@ const DataDisplay = ({ data, ...rest }) => {
   } = rest;
 
   // Un objeto key/value renderea llave y un DataDisplay recursivo con el valor
-  if (isPlainObject(data)) {
-    const dataKeys = keys(keyLabels || data).filter(key => has(data, key));
+  if (_.isPlainObject(data)) {
+    const dataKeys = _.keys(keyLabels || data).filter(key => _.has(data, key));
     return dataKeys.map(key => (
       <Row key={key}>
         <Col xs="3">
-          <Label>{get(keyLabels || {}, key, key)}</Label>
+          <Label>{_.get(keyLabels || {}, key, key)}</Label>
         </Col>
         <Col>
           <DataDisplay data={data[key]} {...rest} />
@@ -36,7 +31,7 @@ const DataDisplay = ({ data, ...rest }) => {
   }
 
   // Una lista de valores renderea DataDisplay recursivamente
-  else if (isArray(data)) {
+  else if (_.isArray(data)) {
     return (
       <Row>
         {data.map(value => (
@@ -49,12 +44,12 @@ const DataDisplay = ({ data, ...rest }) => {
   }
 
   // Un Boolean renderea el texto "Sí" o "No"
-  else if (isBoolean(data)) {
+  else if (_.isBoolean(data)) {
     return <DataDisplay data={data ? 'Sí' : 'No'} {...rest} />;
   }
 
   // Un String renderea un párrafo
-  else if (isString(data)) {
+  else if (_.isString(data)) {
     return <RawHtml as="span">{data}</RawHtml>;
   }
 
