4c2588ac067b5c91d6c677e3355e7c89589a6ad0
David Regla <dreglad@gmail.com>
2019-12-04 05:42:14 -0600

    :sparkles: Descarga de plan de acción

--- src/components/Compromiso/CompromisoList.js
+++ src/components/Compromiso/CompromisoList.js
@@ -2,34 +2,58 @@ import React from 'react';
 import { loader } from 'graphql.macro';
 import { useQuery } from '@apollo/react-hooks';
 import LoadingIndicator from '../LoadingIndicator';
-
+import IconButton from '@material-ui/core/IconButton';
+import Fab from '@material-ui/core/Fab';
+import DownloadIcon from '@material-ui/icons/GetApp';
 import Grid from '@material-ui/core/Grid';
 import Sortable from '../Sortable';
+import Editable from '../Editable';
+
 import CompromisoCard from './CompromisoCard';
 
 const LIST_QUERY = loader('../../queries/CompromisoList.graphql');
 
 const CompromisoList = ({ where }) => {
-  const { data: { items } = {}, loading, error, refetch } = useQuery(
+  const { data: { plan: [plan] = [] } = {}, loading, error, refetch } = useQuery(
     LIST_QUERY,
     {
       variables: {
-        where
+        compromisosWhere: where
       },
       fetchPolicy: 'cache-and-network'
     }
   );
 
   if (error) return <div>Error</div>;
-  if (loading && !items) return <LoadingIndicator />;
-  if (!items) return null;
+  if (loading && !plan) return <LoadingIndicator />;
+  if (!plan) return null;
+
+  const { compromisos = [], metadatos = {} } = plan;
 
   return (
     <div className="vertical-margin-bottom">
       <h2>Compromisos</h2>
       <hr className="line" />
+      <Editable
+        upload
+        item={plan}
+        path="metadatos.descarga"
+        uploadType="file"
+        onUpdate={refetch}
+      >
+        <Fab
+          href={metadatos.descarga}
+          download
+          target="_blank"
+          variant="extended"
+          color="light"
+        >
+          <DownloadIcon title="Descarga el plan de acción" />
+          Descarga plan de acción
+        </Fab>
+      </Editable>
       <Sortable
-        items={items}
+        items={compromisos}
         itemComponent={CompromisoCard}
         itemProps={{}}
         containerComponent={Grid}
