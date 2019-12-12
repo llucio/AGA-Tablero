d82db78b36c399d3bcf87c3a5dccf65955687e15
Marcos Garcia (Dev81) <programador81@funcionpublica.gob.mx>
2019-12-12 16:32:25 -0600

    Header customizable by image cover

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, useParams } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import Container from 'react-bootstrap/Container';
 import Box from '@material-ui/core/Box';
@@ -14,6 +14,7 @@ import CompromisoDetail from './components/Compromiso/CompromisoDetail';
 import HitoDetail from './components/Hito/HitoDetail';
 import ScrollToTop from './components/ScrollToTop';
 import BreadcrumbBar from './components/Layout/BreadcrumbBar';
+import CustomHeader from './components/Layout/CustomHeader';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -31,12 +32,15 @@ const routes = [
     path: '/compromiso/:id',
     content: CompromisoDetail,
     heading: '¡Conoce los avances de los compromisos de Gobierno Abierto!',
+    subheading: '',
+    image: '/assets/images/banner.jpg',
     headerClass: 'medium'
   },
   {
     path: ['/accion-clave/:id'],
     content: HitoDetail,
     heading: '¡Conoce los avances de los compromisos de Gobierno Abierto!',
+    image: '/assets/images/banner.jpg',
     headerClass: 'medium'
   }
 ];
@@ -59,71 +63,63 @@ const App = () => (
   </AuthProvider>
 );
 
-const AppRouter = () => (
-  <Router>
-    <ScrollToTop />
-    {routes.map(
-      ({
-        path,
-        content: Content,
-        heading,
-        subheading,
-        headerClass,
-        image = '/assets/images/decorativa2.jpg',
-        headerArrow = false,
-        exact = true
-      }) => (
-        <Route
-          key={path}
-          path={path}
-          exact={exact}
-          component={props => (
-            <React.Fragment>
-              <MenuPrincipal />
-              {!!heading && (
-                <Heading
-                  heading={heading}
-                  subheading={subheading}
-                  headerArrow={headerArrow}
-                  image={image}
-                  className={headerClass}
-                />
-              )}
+const AppRouter = () => {
+  //let { id } = useParams();
 
-              <section id="one" className="vertical-margin-top-middle">
-                <Container>
-                  <BreadcrumbBar {...props} />
-                  <Content {...props} />
-                </Container>
-              </section>
+  return (
+    <Router>
+      <ScrollToTop />
+      {routes.map(
+        ({
+          path,
+          content: Content,
+          heading,
+          subheading,
+          headerClass,
+          image,
+          //image = '/assets/images/decorativa2.jpg',
+          headerArrow = false,
+          exact = true
+        }) => (
+          <Route
+            key={path}
+            path={path}
+            exact={exact}
+            component={props => (
+              <React.Fragment>
+                <MenuPrincipal />
+                {!!heading && (
+                  <Heading
+                    heading={heading}
+                    subheading={subheading}
+                    headerArrow={headerArrow}
+                    image={image}
+                    className={headerClass}
+                  />
+                )}
 
-              <Footer />
-            </React.Fragment>
-          )}
-        />
-      )
-    )}
-  </Router>
-);
+                <section id="one" className="vertical-margin-top-middle">
+                  <Container>
+                    <BreadcrumbBar {...props} />
+                    <Content {...props} />
+                  </Container>
+                </section>
 
-const Heading = ({ className, image, heading, subheading, headerArrow }) => (
-  <section
-    id="banner"
-    className={className}
-    style={{ backgroundImage: `url(${image})` }}
-  >
-    <Box className="content">
-      <header>
-        <h2 className="big shadow-text">{heading}</h2>
-        {subheading && <h4 className="mt-4 lead shadow-text">{subheading}</h4>}
-      </header>
-    </Box>
-    {headerArrow && (
-      <a href="#one" className="goto-next scrolly">
-        Siguiente
-      </a>
-    )}
-  </section>
-);
+                <Footer />
+              </React.Fragment>
+            )}
+          />
+        )
+      )}
+    </Router>
+  );
+};
+
+const Heading = ({ className, image, heading, subheading, headerArrow, headerClass }) => {
+  //const compromisoId = match.params.id;
+  let { id } = useParams();
+
+  return <CustomHeader compromisoId={id} heading={heading} subheading={subheading} headerArrow={headerArrow} />;
+};
 
 export default App;
---
+++ src/components/Layout/CustomHeader.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { loader } from 'graphql.macro';
+import { useQuery } from '@apollo/react-hooks';
+import LoadingIndicator from '../LoadingIndicator';
+import { LinkContainer } from 'react-router-bootstrap';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Hidden from '@material-ui/core/Hidden';
+import Box from '@material-ui/core/Box';
+
+const GET_QUERY = loader('../../queries/CompromisoGet.graphql');
+
+const CustomHeader = props => {
+  const idCompromiso = props.compromisoId;
+  const heading = props.heading;
+  const subheading = props.subheading;
+  const headerArrow = props.headerArrow;
+
+  const { data: { item } = {}, loading, error } = useQuery(GET_QUERY, {
+    variables: {
+      id: idCompromiso
+    }
+  });
+
+  if (error) return <HeadingDefault />;
+  if (loading && !item) return <LoadingIndicator />;
+  if (!item) return <HeadingDefault />;
+
+  {
+    /*return <>backgroundImage: `url(${item.metadatos.imagen})`</>;*/
+  }
+
+  const imageUrl = item.metadatos.imagen ? item.metadatos.imagen : props.image;
+
+  return (
+    <section id="banner" className="medium" style={{ backgroundImage: `url(${imageUrl})` }}>
+      <Box className="content image-content-- pattern5">
+        <header>
+          <h2 className="big shadow-text">{heading}</h2>
+          {subheading && <h4 className="mt-4 lead shadow-text">{subheading}</h4>}
+        </header>
+      </Box>
+      {headerArrow && (
+        <a href="#one" className="goto-next scrolly">
+          Siguiente
+        </a>
+      )}
+    </section>
+  );
+};
+
+const HeadingDefault = props => {
+  const heading = '¡Conoce los avances de los compromisos de Gobierno Abierto!';
+  const subheading = '';
+  const image = '/assets/images/planes_de_accion.jpg';
+
+  return (
+    <section id="banner" className="medium" style={{ backgroundImage: `url(${image})` }}>
+      <Box className="content image-content-- pattern5">
+        <header>
+          <h2 className="big shadow-text">{heading}</h2>
+          {subheading && <h4 className="mt-4 lead shadow-text">{subheading}</h4>}
+        </header>
+      </Box>
+    </section>
+  );
+};
+
+export default CustomHeader;
