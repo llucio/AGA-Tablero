a7697f08b8a7f1639a64f49ea1335d432ab80a7a
David Regla <dreglad@gmail.com>
2019-12-21 18:22:03 -0600

    :fire: Eliminar dependencias innecesarias

--- src/components/Hito/HitoCard.js
+++ src/components/Hito/HitoCard.js
@@ -20,7 +20,7 @@ const useStyles = makeStyles(theme => ({
     fontWeight: theme.typography.fontWeightRegular
   },
   extendedIcon: {
-    marginRight: teme.spacing(1)
+    marginRight: theme.spacing(1)
   }
 }));
 
