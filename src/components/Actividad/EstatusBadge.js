42539e9f5295e80665605bc669e68815e8bd2527
David Regla <dreglad@gmail.com>
2020-06-12 00:26:10 -0500

    Añadir mutación para cambiar estado

--- src/components/Actividad/EstatusBadge.js
+++ src/components/Actividad/EstatusBadge.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import Fab from '@material-ui/core/Fab';
 import Tooltip from '@material-ui/core/Tooltip';
 import DoneIcon from '@material-ui/icons/Done';
@@ -8,22 +9,54 @@ const ESTATUSES = {
   ninguno: {
     icon: <DoneIcon />,
     title: 'Por iniciar',
-    fabClassName: 'grey lighten-2 white-text'
+    fabClassName: 'grey lighten-2 white-text',
+    siguiente: 'iniciado',
   },
   iniciado: {
     icon: <DoneAllIcon />,
     title: 'En proceso',
-    fabClassName: 'amber white-text'
+    fabClassName: 'amber white-text',
+    siguiente: 'completo',
   },
   completo: {
     icon: <DoneAllIcon />,
     title: 'Completo',
-    fabClassName: 'lime darken-2 white-text'
-  }
+    fabClassName: 'lime darken-2 white-text',
+    siguiente: 'verificado',
+  },
+  verificado: {
+    icon: <DoneAllIcon />,
+    title: 'verificado',
+    fabClassName: 'lime darken-4 white-text',
+  },
 };
 
-const EstatusTooltip = ({ estatus = 'ninguno' }) => {
-  const { icon, title, fabClassName } = ESTATUSES[estatus];
+const EstatusTooltip = ({ actividad, refetch }) => {
+  const estatus = actividad.metadatos.estatus || 'ninguno';
+
+  const { icon, title, fabClassName, siguiente } = ESTATUSES[estatus];
+
+  const [executeChangeEstatus] = useMutation(gql`
+    mutation ChangeActiviadEstatus($id: uuid!, $estatus: String!) {
+      update_actividad(
+        where: { id: { _eq: $id } }
+        _append: { metadatos: { estatus: $estatus } }
+      ) {
+        affected_rows
+      }
+    }
+  `);
+
+  const handleChangeEstatus = (newEstatus) => {
+    executeChangeEstatus({ id: actividad.id, estatus: newEstatus })
+      .then(() => {
+        // El estatus se actualizó correctamente, recargar datos
+        refetch();
+      })
+      .catch((err) => {
+        console.error('Ocurrió error al cambiar estatus: ', err);
+      });
+  };
 
   return (
     <div>
@@ -33,6 +66,11 @@ const EstatusTooltip = ({ estatus = 'ninguno' }) => {
           aria-label="status"
           style={{ margin: '5px' }}
           className={fabClassName}
+          onClick={() => {
+            if (siguiente) {
+              handleChangeEstatus(siguiente);
+            }
+          }}
         >
           {icon}
         </Fab>
