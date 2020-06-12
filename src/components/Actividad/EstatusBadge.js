61e3cd7df6e9274376b93e6a81ac28bf09080887
Ricardo Sánchez <programador06@funcionpublica.gob.mx>
2020-06-12 17:28:50 -0500

    se agrega navegación a fab de  estatus, en medios de verificación de la actividad

--- src/components/Actividad/EstatusBadge.js
+++ src/components/Actividad/EstatusBadge.js
@@ -1,42 +1,61 @@
-import React from 'react';
-import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
-import Fab from '@material-ui/core/Fab';
-import Tooltip from '@material-ui/core/Tooltip';
-import DoneIcon from '@material-ui/icons/Done';
-import DoneAllIcon from '@material-ui/icons/DoneAll';
+import React, { Fragment } from "react";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
+import Fab from "@material-ui/core/Fab";
+import Tooltip from "@material-ui/core/Tooltip";
+import DoneIcon from "@material-ui/icons/Done";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width:150,
+    height:50,
+  },
+}));
 
 const ESTATUSES = {
   ninguno: {
     icon: <DoneIcon />,
-    title: 'Por iniciar',
-    fabClassName: 'grey lighten-2 white-text',
-    siguiente: 'iniciado',
+    title: "Por iniciar",
+    fabClassName: "grey lighten-2 white-text",
+    siguiente: "iniciado",
+    anterior: " ",
   },
   iniciado: {
     icon: <DoneAllIcon />,
-    title: 'En proceso',
-    fabClassName: 'amber white-text',
-    siguiente: 'completo',
+    title: "En proceso",
+    fabClassName: "amber white-text",
+    siguiente: "completo",
+    anterior: "ninguno",
   },
   completo: {
     icon: <DoneAllIcon />,
-    title: 'Completo',
-    fabClassName: 'lime darken-2 white-text',
-    siguiente: 'verificado',
+    title: "Completo",
+    fabClassName: "lime darken-2 white-text",
+    siguiente: "verificado",
+    anterior: "iniciado",
   },
   verificado: {
     icon: <DoneAllIcon />,
-    title: 'verificado',
-    fabClassName: 'lime darken-4 white-text',
-    siguiente: 'ninguno',
+    title: "verificado",
+    fabClassName: "lime darken-4 white-text",
+    siguiente: " ",
+    anterior: "completo",
   },
 };
 
 const EstatusTooltip = ({ actividad, refetch }) => {
-  const estatus = actividad.metadatos.estatus || 'ninguno';
+  const classes = useStyles();
 
-  const { icon, title, fabClassName, siguiente } = ESTATUSES[estatus];
+  const estatus = actividad.metadatos.estatus || "ninguno";
+
+  const { icon, title, fabClassName, siguiente, anterior } = ESTATUSES[estatus];
 
   const [executeChangeEstatus] = useMutation(gql`
     mutation ChangeActiviadEstatus($id: uuid!, $metadatos: jsonb!) {
@@ -49,8 +68,9 @@ const EstatusTooltip = ({ actividad, refetch }) => {
     }
   `);
 
-  console.log(actividad.id);
+  //console.log(actividad.id);
   const handleChangeEstatus = (newEstatus) => {
+    console.log("newEstatus: " + newEstatus);
     executeChangeEstatus({
       variables: {
         id: actividad.id,
@@ -64,27 +84,47 @@ const EstatusTooltip = ({ actividad, refetch }) => {
         refetch();
       })
       .catch((err) => {
-        console.error('Ocurrió error al cambiar estatus: ', err);
+        console.error("Ocurrió error al cambiar estatus: ", err);
       });
   };
 
   return (
-    <div>
-      <Tooltip title={title} placement="right">
+    <div className={classes.root}>
+      <Tooltip title={title} placement="top">
         <Fab
           size="small"
           aria-label="status"
-          style={{ margin: '5px' }}
+          style={{ margin: "10px" }}
           className={fabClassName}
-          onClick={() => {
-            if (siguiente) {
-              handleChangeEstatus(siguiente);
-            }
-          }}
         >
           {icon}
         </Fab>
       </Tooltip>
+
+      <ButtonGroup variant="text" color="primary" aria-label="text primary button group">
+        <Button
+              color="secondary"
+              size="small"
+              onClick={() => {
+                if (anterior) {
+                  handleChangeEstatus(anterior);
+                }
+              }}
+              disabled={title === "Por iniciar"}
+        >{anterior}</Button>
+        
+
+        <Button
+            color="primary"
+            size="small"
+            onClick={() => {
+              if (siguiente) {
+                handleChangeEstatus(siguiente);
+              }
+            }}
+            disabled={title === "verificado"}
+          >{siguiente}</Button>
+      </ButtonGroup>
     </div>
   );
 };
