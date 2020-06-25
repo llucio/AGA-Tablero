9578240aaaaf00c663ec2403195b8e5b99aae4ad
David Regla <dreglad@gmail.com>
2020-06-25 14:19:27 -0500

    Fix Apollo fetch

--- src/apollo.js
+++ src/apollo.js
@@ -4,39 +4,34 @@ import jwt from 'jsonwebtoken';
 import { AUTH_TOKEN_STORAGE_KEY } from './constants';
 
 const uri = process.env.REACT_APP_API_URL || 'http://localhost:4000/v1/graphql';
-const link = new BatchHttpLink({ uri, batchInterval: 20 });
-const cache = new InMemoryCache();
 
 const customFetch = (uri, options) => {
   const token = window.localStorage.getItem(AUTH_TOKEN_STORAGE_KEY);
 
   if (token) {
     const decoded = jwt.decode(token);
-    if (!decoded) return console.error('Error decodificando token:', token);
-
-    let role = 'user';
-    if (
-      decoded['https://hasura.io/jwt/claims'][
-        'x-hasura-allowed-roles'
-      ].includes('administrador')
-    ) {
-      role = 'administrador';
+    if (!decoded) {
+      return console.error('Error decodificando token:', token);
     }
 
     const expired = Math.floor(Date.now() / 1000) >= decoded.exp;
-
     if (!expired) {
       options.headers.Authorization = `Bearer ${token}`;
-      options.headers['x-hasura-role'] = role;
+      const isAdmin = decoded['https://hasura.io/jwt/claims'][
+        'x-hasura-allowed-roles'
+      ].includes('administrador');
+      options.headers['x-hasura-role'] = isAdmin ? 'administrador' : 'user';
     }
   }
 
   return fetch(uri, options);
 };
 
+const link = new BatchHttpLink({ uri, batchInterval: 20, fetch: customFetch });
+const cache = new InMemoryCache();
+
 export const apolloClient = new ApolloClient({
   cache,
   link,
-  queryDeduplication: false,
-  fetch: customFetch,
+  queryDeduplication: true,
 });
